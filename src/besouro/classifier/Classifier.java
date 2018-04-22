913ecba0809e0430c02aa89c8d7ee50808675bfa
Altieres de Matos <altitdb@gmail.com>
2018-04-22 19:22:30 -0300

    Adding new classifiers

---
+++ src/besouro/classifier/Classifier.java
@@ -0,0 +1,9 @@
+package besouro.classifier;
+
+import besouro.model.Episode;
+
+public interface Classifier {
+
+	void classify(Episode episode);
+	
+}
---
+++ src/besouro/classifier/EpisodeClassifier.java
@@ -0,0 +1,11 @@
+package besouro.classifier;
+
+import besouro.model.Episode;
+
+public class EpisodeClassifier implements Classifier {
+
+	public void classify(Episode episode) {
+
+	}
+
+}
