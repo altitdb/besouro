e84f402a927d47b357e3896d9ab1b98ce03d73ce
Altieres de Matos <altitdb@gmail.com>
2018-04-23 22:05:13 -0300

    Update Classifier

--- src/besouro/classifier/EpisodeClassifier.java
+++ src/besouro/classifier/EpisodeClassifier.java
@@ -10,9 +10,29 @@ public class EpisodeClassifier implements Classifier {
 	public Episode classify(List<Action> actions) {
 		System.out.println(actions);
 		
-		TestAdditionClassifier testAddition = new TestAdditionClassifier();
-		Episode episode = testAddition.classify(actions);
-				
+		TestDrivenDevelopmentClassifier testDrivenDevelopment = new TestDrivenDevelopmentClassifier();
+		Episode episode = testDrivenDevelopment.classify(actions);
+		
+		if (episode == null) {
+			TestFirstClassifier testFirst = new TestFirstClassifier();
+			episode = testFirst.classify(actions);
+		}
+		
+		if (episode == null) {
+			TestLastClassifier testLast = new TestLastClassifier();
+			episode = testLast.classify(actions);
+		}
+		
+		if (episode == null) {
+			TestAdditionClassifier testAddition = new TestAdditionClassifier();
+			episode = testAddition.classify(actions);
+		}
+			
+		if (episode == null) {
+			RefactoringClassifier refactoring = new RefactoringClassifier();
+			episode = refactoring.classify(actions);
+		}
+		
 		if (episode == null) {
 			ProductionClassifier production = new ProductionClassifier();
 			episode = production.classify(actions);
